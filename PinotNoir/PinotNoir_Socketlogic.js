299108d31f467015321eaa7e7ef09924cc9d189f
Alexander Henka <Alexander.Henka@googlemail.com>
2015-12-02 15:39:40 +0100

    Extend finches to work with the prefSets

--- PinotNoir/PinotNoir_Socketlogic.js
+++ PinotNoir/PinotNoir_Socketlogic.js
@@ -6,8 +6,6 @@
 
 exports.startTheMagic = function(io){
 
-   //var io = require("socket.io").listen(withServer);
-
     io.sockets.on('connection', function (socket) {
 
         console.log('On Connection');
@@ -17,8 +15,22 @@ exports.startTheMagic = function(io){
         console.log('Emitting new settings update to the clients.');
         console.dir(data);
         console.log('++++++++++++++++++++++++++++++++++');
-      //  socket.broadcast.emit('settings_update', data);
-        socket.broadcast.emit('gpii_update', data);
+
+            var _jf   = require('jsonfile'),
+                _path = require('path'),
+                _persona_name = data.selected_persona,
+                _path_to_pref_set =  _path.join(__dirname, 'Preferences',_persona_name+".json");
+
+                console.log('path to prefernce set = '+ _path_to_pref_set);
+
+                var _jsonData = _jf.readFileSync(_path_to_pref_set);
+
+                console.dir(_jsonData.flat.contexts['gpii-default'].preferences['http://registry.gpii.net/common/contentRep']);
+
+               var _descrTyp = _jsonData.flat.contexts['gpii-default'].preferences;
+
+
+            socket.broadcast.emit('gpii_update', _descrTyp);
     });
   });
 };
